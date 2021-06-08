fde47e1930ca4655646c480d7e1d17fc0b342ac4
burak <burak@DESKTOP-S2V7RA0>
2021-06-08 20:38:54 +0300

    New homework Project

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Musteri kaydı olusturuldu: " + " Tc NO:" + customer.TcNo + " Ad: " + customer.customerName + " Soyad: " + customer.customerSurname);
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Musteri kaydı silindi: " + " Tc NO:" + customer.TcNo + " Ad: " + customer.customerName + " Soyad: " + customer.customerSurname);
+        }
+        public void Liste(Customer[] customers)
+        {
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine(customer.customerName + " " + customer.customerSurname + " listed.");
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer musteri = new Customer();
+            musteri.Id = 0;
+            musteri.TcNo = 1111;
+            musteri.customerName = "Burak";
+            musteri.customerSurname = "Even";
+
+            Customer musteri2 = new Customer();
+            musteri2.Id = 1;
+            musteri2.TcNo = 2222;
+            musteri2.customerName = "Ayse";
+            musteri2.customerSurname = "Deniz";
+
+            Customer musteri3 = new Customer();
+            musteri3.Id = 2;
+            musteri3.TcNo = 3333;
+            musteri3.customerName = "Irem";
+            musteri3.customerSurname = "Yakali";
+
+            Customer musteri4 = new Customer();
+            musteri4.Id = 3;
+            musteri4.TcNo = 4444;
+            musteri4.customerName = "Tugba";
+            musteri4.customerSurname = "Serce";
+
+            Customer[] customers = new Customer[]
+            {
+                musteri,musteri2,musteri3,musteri4
+            };
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(musteri);
+            customerManager.Add(musteri2);
+            customerManager.Add(musteri3);
+            customerManager.Add(musteri4);
+            Console.WriteLine("****************");
+            customerManager.Liste(customers);
+            Console.WriteLine("****************");
+            customerManager.Delete(musteri2);
+
+        }
+    }
+}
