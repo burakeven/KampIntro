4e7de722ec8625f31d7c139de19a19f9199c3443
burak <burak@DESKTOP-S2V7RA0>
2021-06-11 12:09:54 +0300

    MyDictionaryDay4Work

---
+++ MyDictionaryDay4/MyDictionary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionaryDay4
+{
+    class MyDictionary<K,V>
+    {
+        K[] keys;
+        V[] values;
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+        public void Add(K key, V value)
+        {
+            K[] tempKeys = keys;
+            V[] tempValues = values;
+
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+            keys[keys.Length - 1] = key;
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+            values[values.Length - 1] = value;
+        }
+         public void PrintAll()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("Key: {0} - Value: {1}", keys[i], values[i]);
+            }
+        }
+    }
+}
---
+++ MyDictionaryDay4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictionaryDay4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> cars = new MyDictionary<int, string>();
+            cars.Add(1, "Mercedes");
+            cars.Add(2, "BMW");
+            cars.Add(3, "Hyundai");
+            cars.Add(4, "Honda");
+            cars.Add(5, "Chevrolet");
+            cars.Add(6, "Fiat");
+
+            cars.PrintAll();
+            Console.ReadLine();
+        }
+    }
+}
